17c9e896426d6f00a8e1aa4130f15f41d761490c
prakhar-glitch <54439226+prakhar-glitch@users.noreply.github.com>
2022-05-10 23:52:17 +0530

    Added the Nav component

--- src/FormComp.jsx
+++ src/FormComp.jsx
@@ -1,6 +1,9 @@
 import React from  'react'
+import AppBar from   '@mui/icons-material'
 class FormComp extends React.Component {
     render() {
-        return 
+        return <AppBar>
+
+        </AppBar>
     }
 }
