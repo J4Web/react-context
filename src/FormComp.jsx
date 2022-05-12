54b1b40b2c6d163b963abb933bed83cdb8f409d0
prakhar-glitch <54439226+prakhar-glitch@users.noreply.github.com>
2022-05-12 22:20:26 +0530

    completed the form

--- src/FormComp.jsx
+++ src/FormComp.jsx
@@ -1,5 +1,4 @@
 import React from  'react'
-import AppBar from   '@mui/icons-material'
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
@@ -7,25 +6,87 @@ import Avatar from '@mui/material/Avatar';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import { withStyles } from '@mui/styles';
-import { green, pink } from '@mui/material/colors';
+import { pink } from '@mui/material/colors';
 import LockIcon from '@mui/icons-material/Lock';
-
-import styles from "./styles/FormStyles"
+import {Typography} from '@mui/material';
+import {Select} from '@mui/material'
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import {Input} from '@mui/material';
+import { FormControlLabel } from '@mui/material';
+import Checkbox from '@mui/material/Checkbox';
+import Button from '@mui/material/Button';
+const styles={
+    
+    container: {
+        padding:"80px",
+        display: 'flex!important',
+        justifyContent:'center!important',
+        alignItems: 'center!important',
+    
+    },
+    topContainer:{
+        display: 'flex!important',
+        justifyContent:'center',
+        flexDirection:"column",
+        alignItems: 'center!important',
+        padding:"40px"
+    },
+    form:{
+        width:'100%',
+        marginTop:"50px"
+    },
+    submit:{
+        minWidth:'100%!important',
+    }
+}
 
 class FormComp extends React.Component {
     render() {
         const {classes}=this.props;
-        return <>
+        return <main >
             <CssBaseline/>
-            <Container maxWidth="sm">
-                <Box xs={{flexGrow:1}}>
-                <Grid container spacing={2}>
+            <Container className={classes.container} >
+                <Box xs={{flexGrow:1}} >
+                <Grid style={{display:'flex',justifyContent:'center',alignItems: 'center'}} container spacing={1}>
                 <Grid item xs={8}>
-                <Paper>
-<Avatar sx={{ bgcolor: pink[500] }}>
+                <Paper  className={classes.topContainer}>
+<Avatar sx={{ bgcolor: pink[500]}}>
 <LockIcon />
 
 </Avatar>
+<Typography variant="h5">
+Sign In
+
+</Typography>
+<Select value='english'>
+    <MenuItem value="english">English</MenuItem>
+    <MenuItem value="French">French</MenuItem>
+    <MenuItem value="Spanish">Spanish</MenuItem>
+</Select>
+<form className={classes.form}>
+<FormControl margin="normal" required fullWidth>
+<InputLabel htmlFor="email">
+Email
+</InputLabel>
+<Input id="email" name="email" autoFocus>
+
+</Input>
+
+</FormControl>
+<FormControl margin="normal" required fullWidth>
+<InputLabel htmlFor="password">
+Password
+</InputLabel>
+<Input id="password" name="password" >
+
+</Input>
+
+</FormControl>
+<FormControlLabel control={<Checkbox/>} label="Remember Me"/>
+<Button variant="contained" type="submit" color="primary" className={classes.submit}>Sign In</Button>
+</form>
                 </Paper>
 
                 </Grid>
@@ -34,8 +95,7 @@ class FormComp extends React.Component {
                 </Box>
             </Container>
 
-       </>
-    
+       </main>
     }
 }
 export default withStyles(styles)(FormComp)
