315c9996b477a13e0b3ce5f787fc0d6314b4ed08
prakhar-glitch <54439226+prakhar-glitch@users.noreply.github.com>
2022-05-14 23:39:23 +0530

    added the bg comp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import NavBar from "./NavBar";
 import FormComp from "./FormComp";
+import PageContent from "./PageContent";
 function App() {
   return (
     <div className="App">
+    <PageContent>
+
+    
     <NavBar/>
     <FormComp/>
+    </PageContent>
     </div>
   );
 }
